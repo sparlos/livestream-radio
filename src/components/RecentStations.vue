f833e02c803571eb772b5f1914c29b4efdaf103f
Stephen Sparling <mista_fuzzykins@hotmail.com>
2019-08-02 19:41:22 -0400

    fix stations overflow issue

--- src/components/RecentStations.vue
+++ src/components/RecentStations.vue
@@ -1,37 +1,40 @@
 <template>
   <v-container fluid mb-4>
     <v-layout wrap justify-start px-5>
-      <v-flex xs12 offset-xs1 my-4 font-weight-medium class="text--underline">Your Stations</v-flex>
-      <v-flex
-        v-for="(station, i) in stations"
-        :key="i"
-        shrink
-        mr-4
-        :offset-xs1="i===0 ? true : false"
-      >
-        <v-hover v-slot:default="{ hover }">
-          <div class="thumbnail" :title="station.name">
-            <v-img
-              :src="station.imageUrl"
-              aspect-ratio="1"
-              height="80"
-              @click="$emit('changeStation', station)"
-            ></v-img>
-            <div class="thumbnail__more">
-              <ContextMenu 
-                color="white" 
-                type="station"
-                @deleteStation="handleDeleteStation" 
-                @addToSet="handleAddToSet"
-                :station="station" 
-                :stationIndex="i"></ContextMenu>
-            </div>
-            <div class="thumbnail__play" v-if="hover" @click="$emit('changeStation', station, i)">
-              <v-icon color="white">play_circle_filled</v-icon>
-            </div>
-            <div class="thumbnail__text subtitle-2" v-if="hover">{{station.name}}</div>
-          </div>
-        </v-hover>
+      <v-flex xs12 my-4 offset-xs1 font-weight-medium class="text--underline">Your Stations</v-flex>
+      <v-flex d-flex offset-xs1>
+        <v-layout wrap justify-start class="stations">
+          <v-flex v-for="(station, i) in stations" :key="i" shrink mr-4 mb-8>
+            <v-hover v-slot:default="{ hover }">
+              <div class="thumbnail" :title="station.name">
+                <v-img
+                  :src="station.imageUrl"
+                  aspect-ratio="1"
+                  height="80"
+                  @click="$emit('changeStation', station)"
+                ></v-img>
+                <div class="thumbnail__more">
+                  <ContextMenu
+                    color="white"
+                    type="station"
+                    @deleteStation="handleDeleteStation"
+                    @addToSet="handleAddToSet"
+                    :station="station"
+                    :stationIndex="i"
+                  ></ContextMenu>
+                </div>
+                <div
+                  class="thumbnail__play"
+                  v-if="hover"
+                  @click="$emit('changeStation', station, i)"
+                >
+                  <v-icon color="white">play_circle_filled</v-icon>
+                </div>
+                <div class="thumbnail__text subtitle-2" v-if="hover">{{station.name}}</div>
+              </div>
+            </v-hover>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -50,16 +53,21 @@ export default {
   },
   methods: {
     handleDeleteStation(stationIndex, snackbarText, snackbarButton) {
-      this.$emit('deleteStation', stationIndex, snackbarText, snackbarButton);
+      this.$emit("deleteStation", stationIndex, snackbarText, snackbarButton);
     },
     handleAddToSet(station) {
-      this.$emit('addToSet', station);
+      this.$emit("addToSet", station);
     }
   }
 };
 </script>
 
 <style scoped lang='scss'>
+.stations {
+  max-height: 150px;
+  overflow-y: scroll;
+}
+
 .text--underline {
   text-decoration: underline;
 }
